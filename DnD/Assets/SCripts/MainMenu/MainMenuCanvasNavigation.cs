ad3f163f22ee105acc2bae42ab4490e3045bcaa6
Martin Robertson <nfsrob@gmail.com>
2016-11-15 20:28:16 +0000

    First SCene INIT
    :cake:  got some stuff done init

---
+++ DnD/Assets/SCripts/MainMenu/MainMenuCanvasNavigation.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+public class MainMenuCanvasNavigation : MonoBehaviour {
+
+    float leftMove = 0.0f;
+    float transferTime = 0.5f;
+    Vector3 p = Vector3.zero;
+    [SerializeField]
+    Transform MainMenu = null;
+    Vector3 mmStartValue;
+    [SerializeField]
+    Transform HostGameMenu1 = null;
+
+	// Use this for initialization
+	void Start () {
+        mmStartValue = MainMenu.GetComponent<RectTransform>().position;
+        p = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height / 2, Camera.main.nearClipPlane));
+        leftMove = mmStartValue.x - p.x;
+        HostGameMenu1.position += new Vector3(-leftMove * 2,0,0);
+	}
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void BackToMenuFromHostGame()
+    {
+        Sequence mysequence = DOTween.Sequence();
+
+        mysequence.Append(HostGameMenu1.DOMoveX(-leftMove, transferTime, false));
+        mysequence.Append(MainMenu.DOMoveX(leftMove, transferTime, false));
+    }
+
+    #region host game functions
+    public void MainMenuToHostGameMenu1()
+    {
+        Sequence mysequence = DOTween.Sequence();
+
+        mysequence.Append(MainMenu.DOMoveX(-leftMove, transferTime, false));
+        mysequence.Append(HostGameMenu1.DOMoveX(leftMove, transferTime, false));
+    }
+    #endregion
+}
